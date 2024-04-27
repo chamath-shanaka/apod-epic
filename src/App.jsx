e047fdf5c50dbea32f2230a07c146e5d961f8f5f
chamath-shanaka <chamathshanaka19@gmail.com>
2024-04-27 23:32:33 +0530

    Tailwind and DaisyUI fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-
   return (
-    <>
+    <div className="flex flex-col h-screen items-center justify-center">
       <p>app</p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
