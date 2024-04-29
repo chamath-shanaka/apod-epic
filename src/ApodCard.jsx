2dddb4799f72ad5e1df6761b4376d304d294ed4c
chamath-shanaka <chamathshanaka19@gmail.com>
2024-04-29 06:35:59 +0530

    home page hover effect

--- src/ApodCard.jsx
+++ src/ApodCard.jsx
@@ -2,21 +2,24 @@ import bgImg from "./assets/M57Ring_HubbleGendler_3000.jpg";
 
 export default function ApodCard() {
   return (
-    <div
-      className="hero lg:min-h-screen min-h-[50vh]"
-      style={{
-        backgroundImage:
-          `url(${bgImg})`,
-      }}
-    >
-      <div className="hero-overlay bg-opacity-60"></div>
-      <div className="hero-content text-center text-neutral-content">
-        <div className="max-w-md">
-          <h1 className="mb-5 text-5xl font-bold">APOD</h1>
-          <p className="mb-5">
-            Astronomy Picture of the Day.<br />
-            Each day a different image or photograph of our fascinating universe
-          </p>
+    <div className="hero lg:min-h-screen min-h-[50vh] overflow-hidden">
+      <div
+        className="hero lg:min-h-screen min-h-[50vh] hover:scale-[1.04] hover:bg-opacity-60 transition duration-1000"
+        style={{
+          backgroundImage: `url(${bgImg})`,
+        }}
+      >
+        <div className="hero-overlay bg-opacity-40 hover:cursor-pointer"></div>
+        <div className="hero-content text-center text-neutral-200">
+          <div className="max-w-md">
+            <h1 className="mb-5 text-5xl font-bold">APOD</h1>
+            <p className="mb-5">
+              Astronomy Picture of the Day.
+              <br />
+              Each day a different image or photograph of our fascinating
+              universe
+            </p>
+          </div>
         </div>
       </div>
     </div>
--- src/EpicCard.jsx
+++ src/EpicCard.jsx
@@ -2,23 +2,28 @@ import bgImg from "./assets/earth-space-view-canada-0155150d68fb392562e320dd07b9
 
 export default function EpicCard() {
   return (
-    <div
-      className="hero lg:min-h-screen min-h-[50vh]"
-      style={{
-        backgroundImage:
-          `url(${bgImg})`,
-      }}
-    >
-      <div className="hero-overlay bg-opacity-60"></div>
-      <div className="hero-content text-center text-neutral-content">
-        <div className="max-w-md">
-          <h1 className="mb-5 text-5xl font-bold">EPIC</h1>
-          <p className="mb-5">
-            See our blue marble form the Earth-Sun Lagrange point.<br />
-            Imagery collected by DSCOVR's Earth Polychromatic Imaging Camera (EPIC)
-          </p>
+    <div className="hero lg:min-h-screen min-h-[50vh] overflow-hidden">
+      <div
+        className="hero lg:min-h-screen min-h-[50vh] hover:scale-[1.04] hover:bg-opacity-60 transition duration-1000"
+        style={{
+          backgroundImage: `url(${bgImg})`,
+        }}
+      >
+        <div className="hero-overlay bg-opacity-60 hover:cursor-pointer"></div>
+        <div className="hero-content text-center text-neutral-200">
+          <div className="max-w-md">
+            <h1 className="mb-5 text-5xl font-bold">EPIC</h1>
+            <p className="mb-5">
+              See our blue marble form the Earth-Sun Lagrange point.
+              <br />
+              Imagery collected by DSCOVR's Earth Polychromatic Imaging Camera
+              (EPIC)
+            </p>
+          </div>
         </div>
       </div>
     </div>
+
+    
   );
 }
