5871662f845ed78d10829a894a13e6b14a9d10a1
chamath-shanaka <chamathshanaka19@gmail.com>
2024-05-02 17:10:19 +0530

    date picker to EPIC

--- src/EPIC/EpicImgInfoCard.jsx
+++ src/EPIC/EpicImgInfoCard.jsx
@@ -1,9 +1,20 @@
+import { useState } from "react";
+import { DatePicker, CustomProvider } from "rsuite";
+import "rsuite/dist/rsuite.min.css";
+
 export default function () {
+  const [date, setDate] = useState(new Date())
+
   return (
     <div className="h-max w-[90vw] xl:w-[25vw] rounded-md border-solid border-[1px] border-neutral-700">
       <div className="card-body font-mono font-medium">
-        <p className="leading-loose">
-          date: <br /><br />
+
+        <CustomProvider theme="dark">
+          <DatePicker oneTap style={{ width: 200 }} value={date} onSelect={(date) => setDate(date)} />
+        </CustomProvider> 
+
+        <p className="leading-relaxed">
+          <br />
           caption: <br /><br />
           centroid coordinates: <br /><br />
           DSCOVR j2000 position: 150,000,000 km <br /><br />
