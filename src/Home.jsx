f5fe852a193b45893de66ad40fdae3eea58779ea
chamath-shanaka <chamathshanaka19@gmail.com>
2024-05-12 08:29:58 +0530

    feat: authentication

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,11 +1,25 @@
 import ApodHero from "./APOD/ApodHero";
 import EpicHero from "./EPIC/EpicHero";
+import { useKindeAuth } from "@kinde-oss/kinde-auth-react";
+import LoginModal from "./LoginModal";
 
 export default function Home() {
+  const { isAuthenticated, isLoading } = useKindeAuth();
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
   return (
-    <div className="flex flex-col lg:flex-row">
-      <EpicHero />
-      <ApodHero />
-    </div>
+    <>
+      {!isAuthenticated ? (
+        <LoginModal />
+      ) : (
+        <div className="flex flex-col lg:flex-row">
+          <EpicHero />
+          <ApodHero />
+        </div>
+      )}
+    </> 
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,22 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
 import { BrowserRouter } from "react-router-dom";
+import { KindeProvider } from "@kinde-oss/kinde-auth-react";
+const kindeClientId = import.meta.env.VITE_KINDE_CLIENT_ID;
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>,
-)
+    <KindeProvider
+      clientId={kindeClientId}
+      domain="https://apodepic.kinde.com"
+      redirectUri="http://localhost:5173"
+      logoutUri="http://localhost:5173"
+    >
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </KindeProvider>
+  </React.StrictMode>
+);
