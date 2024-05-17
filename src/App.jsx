c7af9e7404d5cd0a2aff39106226e1d4ef1b6eb0
chamath-shanaka <chamathshanaka19@gmail.com>
2024-05-17 20:59:08 +0530

    removed authentication

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,19 @@ import Apod from "./APOD/Apod";
 import Epic from "./EPIC/Epic";
 import Home from "./Home";
 import { Route, Routes, Navigate } from "react-router-dom";
-import {useKindeAuth} from "@kinde-oss/kinde-auth-react";
+// import {useKindeAuth} from "@kinde-oss/kinde-auth-react";
 
 function App() {
-  const { isAuthenticated, isLoading } = useKindeAuth();
-  if (isLoading || isLoading) {
-    return <div>loading...</div>;
-  }
+  // const { isAuthenticated, isLoading } = useKindeAuth();
+  // if (isLoading || isLoading) {
+  //   return <div>loading...</div>;
+  // }
 
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/Epic" element={isAuthenticated ? <Epic /> : <Navigate to="/" />} />
-      <Route path="/Apod" element={isAuthenticated ? <Apod /> : <Navigate to="/" />} />
+      <Route path="/Epic" element={<Epic />} />
+      <Route path="/Apod" element={<Apod />} />
     </Routes>
   );
 }
--- src/Home.jsx
+++ src/Home.jsx
@@ -1,25 +1,19 @@
 import ApodHero from "./APOD/ApodHero";
 import EpicHero from "./EPIC/EpicHero";
-import { useKindeAuth } from "@kinde-oss/kinde-auth-react";
-import LoginModal from "./LoginModal";
+// import { useKindeAuth } from "@kinde-oss/kinde-auth-react";
+// import LoginModal from "./LoginModal";
 
 export default function Home() {
-  const { isAuthenticated, isLoading } = useKindeAuth();
+  // const { isAuthenticated, isLoading } = useKindeAuth();
 
-  if (isLoading) {
-    return <div>Loading...</div>;
-  }
+  // if (isLoading) {
+  //   return <div>Loading...</div>;
+  // }
 
   return (
-    <>
-      {!isAuthenticated ? (
-        <LoginModal />
-      ) : (
-        <div className="flex flex-col lg:flex-row">
-          <EpicHero />
-          <ApodHero />
-        </div>
-      )}
-    </> 
+    <div className="flex flex-col lg:flex-row">
+      <EpicHero />
+      <ApodHero />
+    </div>
   );
 }
